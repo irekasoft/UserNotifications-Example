853b4d988db7329631fa4a54438a7f5859e536f9
Muhammad Hijazi (MBP, Touch) <hijazi@irekasoft.com>
2017-12-23 02:45:57 +0800

    Initial Commit

---
+++ UserNotification-Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserNotification-Example
+//
+//  Created by Muhammad Hijazi on 23/12/2017.
+//  Copyright © 2017 iReka Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
